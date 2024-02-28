7ed12045e8bf0a185d2b1a6173d28efcb28350b8
pavel <pasha12513@gmail.com>
2024-02-28 20:24:58 +0300

    Файлы по python

---
+++ python/Chapter_7/1.py
@@ -0,0 +1,20 @@
+# cheeses = ['brie', 'gjetost', 'havarti']
+# for cheese in cheeses:
+#     if cheese.startswith('x'):
+#         print("I won't eat anything that starts with 'x' ")
+#         break
+#     else:
+#         print(cheese)
+# else:
+#     print("Didn't find anything taht started with 'x'")
+
+# cheeses = []
+# for cheese in cheeses:
+#     print('This shop has some lovely', cheese)
+#     break
+# else:
+#     print("This is not much of a cheese shot, is it?")
+number_list = []
+for number in range(1, 6):
+    number_list.append(number)
+print(number_list)
