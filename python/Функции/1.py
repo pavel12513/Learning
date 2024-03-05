edbc91a3ad4846b5d5f5e3fcc040ace4bccd1e78
pavel <pasha12513@gmail.com>
2024-03-05 21:00:47 +0300

    Файлы по python

---
+++ python/Функции/1.py
@@ -0,0 +1,25 @@
+# def do_nothing():
+
+# def commentary(color):  #
+#     if color == 'red':
+#         return "It's a tomato."
+#     elif color == 'green':
+#         return "It's a green pepper."
+#     elif color == 'bee purple':
+#         return "I don't know what it is, but only bees can see it."
+#     else:
+#         return "I've never heard of the color " + color + "."
+#
+# comment = commentary('red') # передаем цвет функции commentary, а та уже проверяет по условию и возвращает нужные ответ
+# print(comment)
+
+# Позиционные аргументы
+
+# def menu(wine, entree, dessert):
+#     return {'wine': wine, 'entree': entree, 'desset': dessert}
+#
+# test = menu('chardonnay', 'chicken', 'cake')
+# print(test)
+
+# Аргументы - ключевые слова
+
