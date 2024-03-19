7067f73b190814c5b2dc469e4c98a7bf72d64bf1
pavel <pasha12513@gmail.com>
2024-03-19 22:18:05 +0300

    Немного поправил файлы

--- docker/Сервер/server.py
+++ docker/Сервер/server.py
@@ -1,8 +1,17 @@
 import http.server
-from prometheus_client import start_http_server
+import random
 
+from prometheus_client import start_http_server
+from prometheus_client import Counter
+
+REQUESTS = Counter('hello_worlds_total', 'Hello World request.')
+EXCEPTION = Counter('hello_world_exception_total', 'Exceptions serving Hello World. ')
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        REQUESTS.inc()
+        with EXCEPTION.count_exceptions():
+            if random.random() < 0.2:
+                raise Exception
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
--- docker/Сервер/server_new.py
+++ docker/Сервер/server_new.py
@@ -1,3 +1,4 @@
+import http.server
 from http.server import  HTTPServer, BaseHTTPRequestHandler
 
 class NeuralHTTP(BaseHTTPRequestHandler):
@@ -5,3 +6,9 @@ class NeuralHTTP(BaseHTTPRequestHandler):
     def do_Get(self):
         self.send_response(200)
         self.send_header()
+        self.wfile.write(b"Hello World")
+
+if __name__=="__main__":
+    start_http_server(8000)
+    server = http.server.HTTPServer(('localhost', 8001) MyHandler)
+    server.serve_forever()
